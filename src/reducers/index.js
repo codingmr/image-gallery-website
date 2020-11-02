61bee6d1b1f60fc1b424af2e02a6d42958fc850e
codingmr <codingmroberts@gmail.com>
2020-11-02 16:56:47 +0000

    removed demo

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,3 @@
-import { combineReducers } from 'redux';
-import fuelSavings from './fuelSavingsReducer';
-import { connectRouter } from 'connected-react-router'
-
-const rootReducer = history => combineReducers({
-  router: connectRouter(history),
-  fuelSavings,
-});
-
-export default rootReducer;
+// Set up your root reducer here...
+ import { combineReducers } from 'redux';
+ export default combineReducers;
